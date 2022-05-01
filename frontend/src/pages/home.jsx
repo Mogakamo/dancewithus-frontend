22ac49c3a2bf19c491ec851400af792282a34f41
Bhavik P <bhavikjainhp@gmail.com>
2022-05-01 12:44:54 +0530

    completed landing page and designing other pages. axios remaining

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,6 +1,21 @@
+import "../css/home.css"
+import bgv from "../assets/bgv.mp4"
 function Home(){
     return (
-        <h1>Home Route</h1>
+        <div>
+            <section class="showcase">
+                <header>
+                <h2 class="logo">Am.us</h2>
+                </header>
+                <video src={bgv} muted loop autoPlay></video>
+                <div class="overlay"></div>
+                <div class="text">
+                <h2>Never Stop To </h2> 
+                <h3>Exploring The World</h3>
+                <a href="/signup">Sign Up</a>
+                </div>
+            </section>
+        </div>
     )
 }
 
--- frontend/src/pages/signin.jsx
+++ frontend/src/pages/signin.jsx
@@ -3,13 +3,26 @@ import { Form, Button } from 'react-bootstrap';
 import "bootstrap/dist/css/bootstrap.min.css"
 import "../css/custom.css"
 import { useState } from "react";
+import axios from "axios";
+
 function Signin(){
 
     const[email, setEmail] = useState("")
     const[psw, setPsw] = useState("")
 
     const submitForm = ()=> {
-        console.log(email,psw)
+        axios.post("http://localhost:5000/signin",{
+            userEmail : email,
+            userPassword : psw,
+            auth_key : "nl69RtgOBWM986YRy9bs"
+        })
+        .then(res => {
+            if (res.data["status"] === 1){
+                console.log(res.data)
+            }else if (res.data["status"] === 0){
+                console.log(res.data)
+            }
+        })
     }
     return (
         <div className='center'>
